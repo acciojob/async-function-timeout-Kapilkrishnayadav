a1c0aca3ccd0332a5e5470748353361b7fa6b79b
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-06-19 19:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+let text = document.getElementById("text");
+      let delay = document.getElementById("delay");
+      let btn = document.getElementById("btn");
+      let output = document.getElementById("output");
+
+      function promise1(timeDelay) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            output.innerHTML = `${text.value}`;
+            resolve("successful");
+          }, timeDelay);
+        });
+      }
+      async function handleSubmit(event) {
+          event.preventDefault();
+        if (text.value == "" || delay.value == "") {
+            alert("field is empty")
+            return;
+        }
+        let timeDelay = delay.value;
+        await promise1(timeDelay);
+        text.value = "";
+        delay.value = "";
+      }
